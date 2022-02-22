2735f19b199416ac9ba227713c0f7fd5e1b3d193
Vi <vitalya.permyakov155@gmail.com>
2022-02-23 01:39:08 +0700

    New env and materials

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,54 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'dat.gui'
 
+//cubemap
+const path = 'textures/cube/';
+const format = '.jpg';
+const urls = [
+	path + 'px' + format, path + 'nx' + format,
+	path + 'py' + format, path + 'ny' + format,
+	path + 'pz' + format, path + 'nz' + format
+];
+
+const reflectionCube = new THREE.CubeTextureLoader().load( urls );
+const refractionCube = new THREE.CubeTextureLoader().load( urls );
+refractionCube.mapping = THREE.CubeRefractionMapping;
+
+// SCENE
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x808080);
+scene.background = new THREE.Color(0xEEE5E9)
+scene.fog = new THREE.Fog( 0xEEE5E9, 4, 15 )
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+camera.position.set(3, 2, 0)
+
+// RENDER
+const renderer = new THREE.WebGLRenderer( { antialias: true } )
+renderer.setPixelRatio( window.devicePixelRatio )
+renderer.physicallyCorrectLights = true
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+// CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.target.set(0, 0.7, 0)
+
+const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+hemiLight.position.set( 0, 20, 0 );
+scene.add( hemiLight );
 
 //Ambient Light
-const ambientLight = new THREE.AmbientLight(0xFFFFFF)
-ambientLight.position.set(1, 1, 1)
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5)
 scene.add(ambientLight)
 
 //Ambient Light GUI params
@@ -19,60 +61,69 @@ ambientFolder.add(ambientLight, "visible")
 ambientFolder.add(ambientLight, "intensity", 0.0, 1.0)
 
 //Directional Light
-const dirLight = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-dirLight.position.set(2, 10, 1);
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+directionalLight.position.set( 0.1, 1, 0 ); //default; light shining from top
+directionalLight.castShadow = true;
+scene.add(directionalLight);
 
-scene.add(dirLight);
+//Set up shadow properties for the light
+directionalLight.shadow.mapSize.width = 512; // default
+directionalLight.shadow.mapSize.height = 512; // default
+directionalLight.shadow.camera.near = 0.5; // default
+directionalLight.shadow.camera.far = 500; // default
 
 //Directional Light GUI params
 const dirFolder = gui.addFolder('Directional Light')
-dirFolder.add(dirLight, "visible")
-dirFolder.add(dirLight, "intensity", 0.0, 1.0)
+dirFolder.add(directionalLight, "visible")
+dirFolder.add(directionalLight, "intensity", 0.0, 1.0)
 
-// ground
-const mesh = new THREE.Mesh(
-    new THREE.PlaneGeometry(20, 20),
-    new THREE.MeshPhongMaterial({ color: 0x363636, depthWrite: false })
-);
-mesh.rotation.x = -Math.PI / 2;
+const dirLight = new THREE.DirectionalLight( 0xffffff );
+dirLight.position.set( 3, 10, 10 );
+dirLight.castShadow = true;
+dirLight.shadow.camera.top = 2;
+dirLight.shadow.camera.bottom = - 2;
+dirLight.shadow.camera.left = - 2;
+dirLight.shadow.camera.right = 2;
+dirLight.shadow.camera.near = 0.1;
+dirLight.shadow.camera.far = 40;
+scene.add( dirLight );
+
+// GROUND
+const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+mesh.rotation.x = - Math.PI / 2;
 mesh.receiveShadow = true;
-scene.add(mesh);
+scene.add( mesh );
 
-const grid = new THREE.GridHelper(20, 20, 0xFFFFFF, 0xFFFFFF);
-scene.add(grid);
-
-const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.set(3, 2, 0)
-
-const renderer = new THREE.WebGLRenderer()
-renderer.physicallyCorrectLights = true
-renderer.shadowMap.enabled = true
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.target.set(0, 0.7, 0)
-
-const somemap = new THREE.TextureLoader().load( 'models/Cutter_MOS.png' );
+// MATERIALS
 const texture = new THREE.TextureLoader().load( 'models/Cutter_A.png' );
 const normal = new THREE.TextureLoader().load( 'models/Cutter_N.png' );
-
+const metal = new THREE.TextureLoader().load( 'models/Cutter_M.png' );
+const occlusion = new THREE.TextureLoader().load( 'models/Cutter_O.png' );
+const roug = new THREE.TextureLoader().load( 'models/Cutter_S.png' );
 // immediately use the texture for material creation
 const material = new THREE.MeshStandardMaterial( 
     { 
-        map: texture, 
+        map: texture,
         normalMap: normal,
-        aoMap: somemap
+        roughnessMap: roug, 
+        roughness: 1,
+        aoMap: occlusion,
+        metalnessMap: metal,
+        metalness: 1, 
+        envMap: reflectionCube
     } 
 );
 
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 128, {
+    format: THREE.RGBAFormat,
+    generateMipmaps: true,
+    minFilter: THREE.LinearMipmapLinearFilter,
+    encoding: THREE.sRGBEncoding
+} );
+
+const cubeCamera = new THREE.CubeCamera( 1, 10000, cubeRenderTarget );
+
+// LOADER
 const fbxLoader = new FBXLoader()
 fbxLoader.load(
     'models/cutter.fbx',
@@ -82,8 +133,12 @@ fbxLoader.load(
                 (child as THREE.Mesh).material = material
              }
          })
+         
         object.scale.set(.01, .01, .01)
         object.rotation.set(0, 4, 0)
+        object.castShadow = true //default is false
+        object.receiveShadow = false //default
+        object.add(cubeCamera)
         scene.add(object)
     },
     (xhr) => {
@@ -110,9 +165,12 @@ function animate() {
 
     controls.update()
 
+    cubeCamera.update( renderer, scene );
+
     render()
 
     stats.update()
+
 }
 
 function render() {
